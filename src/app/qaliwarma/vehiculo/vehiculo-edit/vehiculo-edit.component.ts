a0c2868226a9b206e44bfe7b0f519989922a71b4
crojas <crojas.adicse@gmail.com>
2018-07-08 15:11:50 -0500

    se modifico vehiculo edit component

--- src/app/qaliwarma/vehiculo/vehiculo-edit/vehiculo-edit.component.ts
+++ src/app/qaliwarma/vehiculo/vehiculo-edit/vehiculo-edit.component.ts
@@ -60,10 +60,11 @@ export class VehiculoEditComponent implements OnInit {
 
   edit(){
 
+
     this.crudHttpClientServiceShared.edit(this.id,"vehiculo","edit").subscribe(
       res => {
-        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca);
-        this.buildForm();
+        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca,res.marcaVehiculo);
+        this.vehiculoForm.setValue(this.vehiculoModel)
         
       },
       error=>console.log(error),
@@ -77,11 +78,12 @@ export class VehiculoEditComponent implements OnInit {
 
 
   create(){
-    this.crudHttpClientServiceShared.create(this.vehiculoModel,"vehiculo","create").subscribe(
+    let data =  JSON.stringify(this.vehiculoForm.value);
+    this.crudHttpClientServiceShared.create(data,"vehiculo","create").subscribe(
 
       res=>{
-        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca);
-        this.buildForm();
+        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca,res.marcaVehiculo);
+        this.vehiculoForm.setValue(this.vehiculoModel)
         this.flagRefreshReturn = true;
       },
       error=>console.log(error),
@@ -98,12 +100,13 @@ export class VehiculoEditComponent implements OnInit {
   }
 
   update(){
-    this.crudHttpClientServiceShared.update(this.vehiculoModel,"vehiculo","update").subscribe(
+    let data =  JSON.stringify(this.vehiculoForm.value);
+    this.crudHttpClientServiceShared.update(data,"vehiculo","update").subscribe(
 
      
       res=>{
-        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca);
-        this.buildForm();
+        this.vehiculoModel = new VehiculoModel(res.idVehiculo,res.numeroPlaca,res.marcaVehiculo);
+        this.vehiculoForm.setValue(this.vehiculoModel);
         this.flagRefreshReturn = true;
       },
       error=>console.log(error),
